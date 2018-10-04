5bec2f5ddc87b7fed3447df7346be0bd70e34886
froylan.vargas <froylan.vargas.gomez@gmail.com>
2018-10-03 21:14:45 -0500

    First commit of the api

---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const twitter = require('twitter');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const twitterClient = new twitter({
+    consumer_key: 'MTquifSXcAPrU1lH95xnYPGdl',
+    consumer_secret: '1l88tt0EI7Vh2PjXGY4O7Fnu0MphLozkIIQyMH9ekma1MWVpry',
+    bearer_token: 'AAAAAAAAAAAAAAAAAAAAAJ9p8gAAAAAAXm%2FSAdFTl5UyrxfFPnQGYwxo%2FjM%3Du99tgcBLUHT2xToThlbJiB0zmFkqzcXT4ed3mFnDVbIYapzOCz'
+  });
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+
+app.get('/tweets', (req,res) => {
+    const { searchTerm } = req.query;
+    twitterClient.get('search/tweets', {q: searchTerm}, function(error, tweets, response) {
+        res.json(tweets);
+     });
+});
+
+app.listen(4000);
